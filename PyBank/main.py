bebf86c10b50a91234f82743af49420a17799558
DurangoJake <Henry.Andrasovsky@du.edu>
2020-04-05 15:55:01 -0600

    almost done prt1

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,48 +3,63 @@ import csv
 
 file =('budget_data.csv')
 
+output_path =('outputbudget.csv')
+
 with open(file) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
    
     x = 0
     month_adder = 0
+    month_list = []
     y = []
     z = 0 
 
     next (csvreader)
-   
-    
 
-    
     for row in csvreader:
-        
-        
-
     
-    
-        
         x = x + int(row[1])
         month_adder = month_adder + 1
         y.append(row[1])
+        month_list.append(row[0])
+
+
+    length = len(y) - 1
+    new_list = []
+    perchange = 0
+    for u in range(length):
+        changer = int(y[u+1]) - int(y[u])
+        perchange = perchange + changer
+        new_list.append(changer)
         
     
-    print(y)
 
-
-   
-    
-    
+    totalchange = perchange / length
+  
         
+    high = max(new_list)
+    low = min(new_list)
 
-    
-   
-    
+    index_high = new_list.index(high) + 1
+
+    index_low = new_list.index(low) + 1
+
+    print ("Financial Analysis")
+    print ("-"*60)
     print ("Total months: ", month_adder)
     print ("Total: $",  x)
-    print ("Average Change: $", z)
-    print("Greatest Increase in Profits: ")
-    print("Greatest Decrease in Profits: ")
+    print ("Average Change: $", totalchange)
+    print("Greatest Increase in Profits: ", month_list[index_high],  "($",high , ")")
+    print("Greatest Decrease in Profits: ", month_list[index_low], "($",low , ")" )
+
+
+
+with open (output_path, 'w') as csvfile:
+
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    writer.writerow = ([])
     
 
         
